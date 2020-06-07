79b7f1d61ed36dbffc129c2c918a456fbf942bf6
Helen Dias <helendias.hd@gmail.com>
2020-06-07 15:09:52 -0300

    :bento: Add images

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import routes from './routes';
 
@@ -9,3 +10,4 @@ app.listen('4000');
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
