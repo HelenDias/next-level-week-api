b36f391a173702dd677d5008c6262063faa9c5c6
Helen Dias <helendias.hd@gmail.com>
2020-06-07 13:50:22 -0300

    :fire: Remove routes

--- src/server.ts
+++ src/server.ts
@@ -7,35 +7,4 @@ app.listen('4000');
 app.use(express.json());
 app.use(cors());
 
-const users = [
-  'Helen',
-  'Alice',
-  'Yasmin',
-];
 
-app.get('/users', (request, response) => {
-  const search = request.query.search;
-
-  const filteredUsers = !!search
-    ? users.filter(user => user.includes(String(search)))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
