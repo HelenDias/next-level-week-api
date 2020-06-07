e25a4f6355ea0e38fee5fe1ddded61995fb781ec
Helen Dias <helendias.hd@gmail.com>
2020-06-07 13:55:18 -0300

    :sparkles: Add connection file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
 app.listen('4000');
+
 app.use(express.json());
 app.use(cors());
-
-
+app.use(routes);
