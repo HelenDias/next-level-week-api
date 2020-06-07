dd5cc556201332cf431cd4a1cc11499025e08299
Helen Dias <helendias.hd@gmail.com>
2020-06-07 15:17:36 -0300

    :sparkles: Add route to get items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello world' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  return response.json(items.map(item => ({
+    ...item,
+    image: `http://localhost:4000/uploads/${item.image}`,
+  })));
 });
 
 export default routes;
