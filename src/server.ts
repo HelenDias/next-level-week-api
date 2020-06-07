963168ad642ed43c1196da694acba0350ccfddcd
Helen Dias <helendias.hd@gmail.com>
2020-06-07 12:59:13 -0300

    :sparkles: add routes

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,31 @@ const app = express();
 
 app.listen('4000');
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+const users = [
+  'Helen',
+  'Alice',
+  'Yasmin',
+];
 
-  response.json([
-    'Helen',
-    'Alice',
-    'Yasmin',
-  ]);
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Helen',
+    email: 'helen@dev-next.com',
+  };
+
+  return response.json(user);
 });
