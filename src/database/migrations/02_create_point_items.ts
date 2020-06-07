132335f69f0b359eb0d3eedbe633cb9ca533c5ac
Helen Dias <helendias.hd@gmail.com>
2020-06-07 14:41:32 -0300

    :sparkles: Add point items migration

---
+++ src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id')
+      .primary();
+
+    table.string('image')
+      .notNullable();
+
+    table.integer('point_id')
+      .notNullable()
+      .references('id')
+      .inTable('points');
+
+    table.integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('points');
+  });
+};
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
+};
