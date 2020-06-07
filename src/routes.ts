99d4ae1a65b0728f216974a147fa90015a98c192
Helen Dias <helendias.hd@gmail.com>
2020-06-07 15:48:23 -0300

    :sparkles: Add route to add points

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import knex from './database/connection';
+import omit from 'lodash/omit';
 
 const routes = express.Router();
 
@@ -10,6 +11,25 @@ routes.get('/items', async (request, response) => {
     ...item,
     image: `http://localhost:4000/uploads/${item.image}`,
   })));
+});
+
+routes.post('/points', async (request, response) => {
+  const { items } = request.body;
+
+  const ids = await knex('points').insert({
+    ...omit(request.body, 'items'),
+    image: 'image-fake',
+  });
+
+  const pointItems = items.map((item_id: number) => ({
+    item_id,
+    point_id: ids[0],
+    image: 'image-fake',
+  }));
+
+  await knex('point_items').insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
